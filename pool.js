f2a6be8bbcb05f3d8dd5df07a91229541a906417
xiechuntao <306454568@qq.com>
2021-06-12 22:29:58 +0800

    修改

--- pool.js
+++ pool.js
@@ -3,10 +3,11 @@
 var mysql = require('mysql');
 //创建mysql实例
 var pool = mysql.createConnection({
-  host: '127.0.0.1',
+  // host: '127.0.0.1',
+  host: '212.64.32.35',
   port: '3306',
   user: 'root',
   password: 'root',
-  database: 'your database name',
+  database: 'general',
 });
 module.exports = pool;
